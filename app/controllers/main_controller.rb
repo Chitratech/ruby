8a4003b663766bffcce6b56972df9145755662bb
chitra <chitraraj94@gmail.com>
2016-01-14 18:00:47 +0530

    first commit

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,14 @@
+class MainController < ApplicationController
+
+
+
+def welcome
+@num_books=Book.count
+end
+
+def result
+@bookid=params[:sid]
+@bookz=Book:find(:all,:conditions=>"id=#{@bookid}")
+
+end
+end
